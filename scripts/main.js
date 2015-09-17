04ceb4795a0fdf2454855740cdc27b561dfaec06
Jmingus <jmingus@austin.rr.com>
2015-09-17 17:37:48 -0500

    Site responsive, however image logic can't been formed yet

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+$(document).ready(function(){
+  //variables
+  var $form = $('form')
+  //functions
+  //event listeners
+  $('#nav-bar-add').click(function(){
+    $form.toggle();
+  });
+  $('#column-add').click(function(){
+    $form.toggle();
+  })
+  $('#cancel').click(function(){
+    $form.hide();
+  })
+
+});
