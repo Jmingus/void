f2de8736d8d94cb98730b728a70f5e8f5acf2847
Jmingus <jmingus@austin.rr.com>
2015-09-18 11:39:57 -0500

    wrote resetInputs and basic get and post functions

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -2,17 +2,46 @@
 'use strict';
 $(document).ready(function () {
   //variables
+
   var $form = $('form');
+  var $imageLink = $('#image-link');
+  var $imageCaption = $('#image-caption');
   //functions
+
+  function post() {
+    $.post('http://tifye.herokuapp.com/collections/void', { imageLink: $imageLink, imageCaption: $imageCaption }, function (response) {
+      console.log('posted', response);
+    }, 'json');
+  };
+
+  function render() {
+    $.get('http://tifye.herokuapp.com/collections/void', function (response) {
+      console.log(response);
+    });
+  }
+
+  function resetInputs() {
+    $imageLink.val('');
+    $imageCaption.val('');
+  }
   //event listeners
+
   $('#nav-bar-add').click(function () {
     $form.toggle();
+    resetInputs();
   });
   $('#column-add').click(function () {
     $form.toggle();
+    resetInputs();
   });
   $('#cancel').click(function () {
     $form.hide();
+    resetInputs();
+  });
+
+  $('#submit').click(function () {
+    post();
+    render();
   });
 });
 
